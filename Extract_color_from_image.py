fba6bbc03ddf316f4197b7602dc4ef0d0057398a
ko-nishiura <myoko0905@outlook.jp>
2021-10-22 09:40:40 +0900

    Extract_color_from_image

--- Extract_color_from_image.py
+++ Extract_color_from_image.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 
 ## 使用する画像指定 jpgでもpngでも可
-img = cv2.imread("./a.png")
+img = cv2.imread("./original.png")
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 ## hsvで抜き出す色の範囲を指定
 # mask = cv2.inRange(hsv, (36, 0, 0), (86, 255,255))  #green
 # mask = cv2.inRange(hsv, (5, 0, 0), (30, 255,255))  # red
-mask = cv2.inRange(hsv, (100, 0, 0), (120, 255,255)) #blue
+mask = cv2.inRange(hsv, (100, 0, 0), (120, 255, 255))  # blue
 
-imask = mask>0
+imask = mask > 0
 green = np.zeros_like(img, np.uint8)
 green[imask] = img[imask]
 
